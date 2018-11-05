daee27f6d5876d494419a614b82e18107c5c4ef7
Alexander Shepelin <brightcor@gmail.com>
2018-11-05 19:33:39 +0300

    feat: Add google analytics (ga) adapter

---
+++ src/adapters/google-ga.test.ts
@@ -0,0 +1,44 @@
+import { googleGA } from './google-ga';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      ga?: Function;
+    }
+  }
+}
+
+describe('googleGA', () => {
+  it('does nothing if Google Analytics function not found', () => {
+    expect(() => {
+      googleGA('test');
+    }).not.toThrow();
+  });
+
+  it('maps event only', () => {
+    global.ga = jest.fn();
+
+    googleGA('testEvent');
+
+    expect(global.ga).toBeCalledWith('send', 'event', undefined, 'testEvent', undefined, undefined);
+  });
+
+  it('maps event and its properties', () => {
+    global.ga = jest.fn();
+
+    googleGA('testEvent', {
+      category: 'testCategory',
+      label: 'testLabel',
+      value: 'testValue',
+    });
+
+    expect(global.ga).toBeCalledWith(
+      'send',
+      'event',
+      'testCategory',
+      'testEvent',
+      'testLabel',
+      'testValue',
+    );
+  });
+});
---
+++ src/adapters/google-ga.ts
@@ -0,0 +1,31 @@
+import { EventProperties, EventTracker } from '../wrapped-analytics';
+
+// ga function signature:
+// ga('send', 'event', [eventCategory], [eventAction], [eventLabel], [eventValue], [fieldsObject]);
+// see https://developers.google.com/analytics/devguides/collection/analyticsjs/events#implementation
+interface GA {
+  (
+    method: string,
+    type: string,
+    eventCategory?: string,
+    eventAction?: string,
+    eventLabel?: string,
+    fieldsObject?: any,
+  ): void;
+}
+
+declare global {
+  interface Window {
+    ga?: GA;
+  }
+}
+
+export const googleGA: EventTracker = (event: string, eventProperties?: EventProperties): void => {
+  const ga = window.ga;
+  if (!ga) {
+    return;
+  }
+
+  const { category, label, value } = eventProperties || ({} as EventProperties);
+  ga('send', 'event', category, event, label, value);
+};
